6bebd48e1e6bfc927d888c79ed2936f90a3ff8b3
Ezzar616 <103579773+Ezzar616@users.noreply.github.com>
2022-06-27 22:10:02 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,14 +1,12 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [082146092695]
-│ • Gopay  [082146092695]
-│ • Dana  [082146092695]
+│ • Tri [089509083794]
+│ • Gopay  [087853163532]
+│ • Dana  [087855032553]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6282146092695
+│ > Ingin donasi? Wa.me/6287853163532
 │ _Hasil donasi akan digunakan buat sewa_
-│ _atau beli *RDP/VPS* agar bot bisa jalan_
-│ _24jam tanpa kendala_
 ╰────
 `.trim(), wm, 'Menu', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
 handler.help = ['donasi']
